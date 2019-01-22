021682aa1566555240e45e41655632176d29c849
sumitjoshi <sumit.joshi@nbcuni.com>
2019-01-22 18:45:14 +0530

    http

--- angular-test/src/app/hero.service.ts
+++ angular-test/src/app/hero.service.ts
@@ -4,22 +4,54 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+  constructor( private http: HttpClient,
+    private messageService: MessageService) {
+      this.http.get('https://jsonplaceholder.typicode.com').subscribe(data => {
+      console.log(data);
+    });
+     }
  
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
- 
+   /** GET heroes from the server */
+   getHeroes (): Observable<Hero[]> {
+     let res=this.http.get<Hero[]>(this.heroesUrl)
+     .pipe(
+      catchError(this.handleError('getHeroes', []))
+      );
+   
+     return res
+   }
+     /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+     
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+     
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+     
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
+    }
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
 }
